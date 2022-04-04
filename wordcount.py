2dd42566ba0b9a5d092abf80338c86ca4938877a
jessicamow <jessicammow@gmail.com>
2022-04-04 12:04:45 -0700

    Created word count function to count the amount of words in each file

--- wordcount.py
+++ wordcount.py
@@ -1,4 +1,21 @@
 """Count words in file."""
 
+# letter_counts = {}
+
+#     for letter in phrase:
+#         letter_counts[letter] = letter_counts.get(letter, 0) + 1
+
+#     return letter_counts
 
 # put your code here.
+def word_count(filename):
+    file1 = open(filename)
+    word_count = {}
+    for line in file1:
+        words = line.split(' ')
+        for word in words:
+            word_count[word] = word_count.get(word, 0) + 1
+    
+    for word, count in word_count.items():
+        print(word, count)
+ 
